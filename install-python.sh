ecf9c46c1c6024d0a73bfa84cd54b2c9b11df0e2
bigerl <alexander@bigerl.eu>
2025-07-15 13:11:51 +0200

    add install-python.sh and make all shell scripts executable

--- install-python.sh
+++ install-python.sh
@@ -2,8 +2,6 @@
 
 set -eu
 
-TMPDIR=""
-
 fuzzy_eq() {
     local input="$1"
     local pattern="$2"
@@ -12,16 +10,11 @@ fuzzy_eq() {
     return "$?"
 }
 
-cleanup() {
-    if [ -n "$TMPDIR" ]; then
-        rm -rf "$TMPDIR"
-    fi
-}
-
 
 ARCH="$(uname -m)"
 if fuzzy_eq "${ARCH}" "arm64" || fuzzy_eq "${ARCH}" "aarch64"; then
-    ARCH="aarch64"
+    echo "aarch64 is currently not supported"
+    exit 1
 elif fuzzy_eq "${ARCH}" "x86_64" || fuzzy_eq "${ARCH}" "x86-64"; then
     ARCH="x86_64"
 else
@@ -33,7 +26,8 @@ OS="$(uname -o)"
 if fuzzy_eq "${OS}" "linux"; then
     OS="linux"
 elif fuzzy_eq "${OS}" "darwin"; then
-    OS="darwin"
+    echo "Darwin is currently not supported"
+    exit 1
 else
     echo "Unknown operating system"
     exit 1
@@ -44,15 +38,6 @@ REPO="https://github.com/tentris/tentris"
 LATEST_RELEASE_PAGE="$(curl --proto https --tlsv1.2 -Ls -o /dev/null -w "%{url_effective}" "${REPO}/releases/latest")"
 LATEST_RELEASE_VER="${LATEST_RELEASE_PAGE##*/}"
 
-DOWNLOAD_URL="${REPO}/releases/download/${LATEST_RELEASE_VER}/tentris-${LATEST_RELEASE_VER}-beta-${ARCH}-${OS}.tar.gz"
+DOWNLOAD_URL="${REPO}/releases/download/${LATEST_RELEASE_VER}/tentris-python-${LATEST_RELEASE_VER}-beta-${ARCH}-${OS}.tar.gz"
 
-TMPDIR="$(mktemp -d)"
-trap cleanup EXIT
-cd "${TMPDIR}"
-
-echo "Downloading ${DOWNLOAD_URL}"
-curl --proto https --tlsv1.2 -L -o "tentris.tar.gz" "${DOWNLOAD_URL}"
-
-tar xf "tentris.tar.gz"
-
-sh ./install.sh
+pip3 install "$DOWNLOAD_URL"
